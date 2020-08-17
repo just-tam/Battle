75ff6294590f9d13f03f7fa64ad157292fca0b15
Tamara Jeffrey <just-tam@live.com>
2020-08-17 17:45:47 +0100

    added web_helpers.rb and created sign_in_and_play method

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names test' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Bart'
-    fill_in :player_2_name, with: 'Lisa'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Bart vs. Lisa'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Hit points test' do
   scenario 'see player 2 of hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Bart'
-    fill_in :player_2_name, with: 'Lisa'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Lisa: 60HP'
   end
 end
