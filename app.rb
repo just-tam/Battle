d8cda93bcf557a61eb8753358b7a6d82c05a2d08
Tamara Jeffrey <just-tam@live.com>
2020-08-19 13:15:29 +0100

    Added Game class and extracted attack method from player class

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 
 require './lib/player.rb'
+require './lib/game.rb'
+
 
 class Battle < Sinatra::Base
 
@@ -25,7 +27,7 @@ class Battle < Sinatra::Base
   get '/attack' do
     @player_1 = $player_1
     @player_2 = $player_2
-    @player_1.attack(@player_2)
+    Game.new.attack(@player_2)
     erb :attack
   end
 
