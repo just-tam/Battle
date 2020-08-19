8c5c094bc636fd999b35ee9a316d302fb4299652
Tamara Jeffrey <just-tam@live.com>
2020-08-19 14:05:05 +0100

    added ability for players to switch turns, tests passing

--- app.rb
+++ app.rb
@@ -26,7 +26,8 @@ class Battle < Sinatra::Base
 
   get '/attack' do
     @game = $game
-+   @game.attack(@game.player_2)
+    @game.attack(@game.player_2)
+    @game.switch_turns
     erb :attack
   end
 
---
+++ spec/features/switch_turns_spec.rb
@@ -0,0 +1,16 @@
+feature 'Switch turns test' do
+    context 'seeing the current turn' do
+      scenario 'at the start of the game' do
+        sign_in_and_play
+        expect(page).to have_content "Bart's turn"
+      end
+
+      scenario 'after player 1 attacks' do
+        sign_in_and_play
+        click_button 'Attack'
+        click_link 'OK'
+        expect(page).not_to have_content "Bart's turn"
+        expect(page).to have_content "Lisa's turn"
+      end
+    end
+end
